d20ddc6d1d36e1db5afcc8ac7a2e4a6dd78dc593
kanishka <kanishkavijay39@gmail.com>
2022-03-06 17:57:31 +0530

    cryptoCurrency

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from '../components/Banner/Banner'
+import Carousel from '../components/Banner/Carousel'
+import CoinTable from '../components/CoinTable'
+
+const HomePage = () => {
+  return (
+    <>
+    <Banner />
+    <CoinTable />
+   
+    </>
+  )
+}
+
+export default HomePage
